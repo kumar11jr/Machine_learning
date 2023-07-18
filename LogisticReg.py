7fed24be68e096f4a0b09fe01660af7805e1f13b
kumar11jr <kumar11jr@gmail.com>
2023-07-18 20:59:35 +0530

    Logistic_reg

--- LogisticReg.py
+++ LogisticReg.py
@@ -2,3 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+df = pd.read_csv("LogisticReg.csv")
+
+# print(df.head(5))
+
+plt.scatter(df.age,df.bought_insurance,color="red",marker="*")
+# plt.show()
+
+# print(df.shape)
+
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test =train_test_split(df[['age']],df.bought_insurance,test_size=0.1)
+
+# print(X_test)
+
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+model.fit(X_train,y_train)
+print(model.predict(X_test))
+print(model.score(X_test,y_test))
